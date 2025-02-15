8c7951b62369ad297c5bbd8fa0fa17cbefbb05f2
Adithyenkandasamy <adithyen1@gmail.com>
2025-02-15 14:35:16 +0530

    ds

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ class Animal{
     String name;
     void DisplayName(String name){
         System.out.println("The animal name is from animal class:"+name);
-    }
 }
 class Dog extends Animal{
     void setName(String name){
